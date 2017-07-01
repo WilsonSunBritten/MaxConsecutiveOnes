5bc1a1c11530234b1c2fe5b70e39a233ca179f39
WilsonSunBritten <wilsunb@gmail.com>
2017-06-30 19:09:45 -0600

    solution

--- src/maxconsecutiveones/MaxConsecutiveOnes.java
+++ src/maxconsecutiveones/MaxConsecutiveOnes.java
@@ -13,8 +13,22 @@ public class MaxConsecutiveOnes {
         int[] testArray = {0,0,1,1,1,0,1,1,0,1,1,1,1,0,0,1,0,0,1};
     }
     
+    //Values may be assumed to only be ones and zeros.
     public static int findMaxConsecutiveOnes(int[] values){
-        return 0;
+        int currentMax = 0;
+        int max = 0;
+        for (int index = 0; index < values.length; index++) {
+            currentMax = values[index] == 0 ?  0 : currentMax + 1;
+            max = Math.max(max, currentMax);
+        }
+        return max;
     }
     
+    /*Notes on solution: Very simple O(n) solution. I don't believe a 
+    better O solution exists. Possible to cut execution short by checking the
+    remaining length of the array against the current max to see if further 
+    searching is worthwhile, but in many cases the extra conditional would take 
+    more overall execution time than would be saved.
+    
+    */
 }
