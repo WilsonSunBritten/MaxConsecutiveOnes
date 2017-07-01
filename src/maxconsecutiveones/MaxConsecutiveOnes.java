a07d2cdc810d99559ed1582717774a167af3a3b2
WilsonSunBritten <wilsunb@gmail.com>
2017-06-30 19:00:53 -0600

    Initial Setup

---
+++ src/maxconsecutiveones/MaxConsecutiveOnes.java
@@ -0,0 +1,20 @@
+/*
+ * Given a binary array, find the maximum number of consecutive ones
+ */
+package maxconsecutiveones;
+
+/**
+ *
+ * @author Wilson Britten
+ */
+public class MaxConsecutiveOnes {
+
+    public static void main(String[] args) {
+        int[] testArray = {0,0,1,1,1,0,1,1,0,1,1,1,1,0,0,1,0,0,1};
+    }
+    
+    public static int findMaxConsecutiveOnes(int[] values){
+        return 0;
+    }
+    
+}
